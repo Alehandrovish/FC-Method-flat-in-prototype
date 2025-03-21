2e803e2a959ff30691b3745375e8033dee62c1d0
Alex <sascha.rumin@gmail.com>
2025-03-21 14:14:31 +0200

    Fixing code

--- src/index.js
+++ src/index.js
@@ -1,75 +1,77 @@
 "use strict";
 
 class MyArray {
-  constructor(...agrs) {
+  constructor(...args) {
     this.length = 0;
-    for (let i = 0; i < agrs.length; i++) {
-      this.push(agrs[i]);
+    for (let i = 0; i < args.length; i++) {
+      this.push(args[i]);
     }
   }
-  push(...agrs) {
-    if (agrs) {
-      for (let i = 0; i < agrs.length; i++) {
-        this[this.length++] = agrs[i];
-      }
-      return this.length;
-    } else {
-      let err = new RangeError("args is not exist");
-      return err;
-    }
-  }
-  pop() {
-    if (this.length === 0) {
-      return;
-    }
-    const last_item = this.length - 1;
-    delete this[last_item];
-    return last_item;
-  }
-  forEach(func) {
-    if (typeof func !== "function") {
-      return -1;
-    }
-    for (let i = 0; i < this.length; i++) {
-      func(this[i], i, this);
-    }
-  }
-  concat(...arrs) {
-    let newMass = new MyArray();
-    this.forEach((el) => {
-      newMass.push(el);
-    });
-    for (let i = 0; i < arrs.length; i++) {
-      if (Array.isArray(arrs[i])) {
-        newMass.push(...arrs[i]);
-      } else if (MyArray.isMyArray(arrs[i])) {
-        for (let j = 0; j < arrs[i].length; j++) {
-          newMass.push(arrs[i][j]);
-        }
-      } else {
-        newMass.push(arrs[i]);
-      }
-    }
-    return newMass;
-  }
+
   static isMyArray(arr) {
     return arr instanceof MyArray;
   }
+}
 
-  flat(depht = 1) {
-    let newArr = new MyArray();
-    if (MyArray.isMyArray(this)) {
-      this.forEach((el) => {
-        if (MyArray.isMyArray(el) && depht > 0) {
-          newArr = newArr.concat(el.flat(depht - 1));
-        } else {
-          newArr.push(el);
-        }
-      });
-      return newArr;
+MyArray.prototype.push = function (...args) {
+  if (args) {
+    for (let i = 0; i < args.length; i++) {
+      this[this.length++] = args[i];
+    }
+    return this.length;
+  }
+};
+
+MyArray.prototype.pop = function () {
+  if (this.length === 0) {
+    return;
+  }
+  const last_item = this.length - 1;
+  delete this[last_item];
+  return last_item;
+};
+
+MyArray.prototype.forEach = function (func) {
+  if (typeof func !== "function") {
+    throw new TypeError("Callback must be a function");
+  }
+  for (let i = 0; i < this.length; i++) {
+    func(this[i], i, this);
+  }
+};
+
+MyArray.prototype.concat = function (...arrs) {
+  let newMass = new MyArray();
+  this.forEach((el) => {
+    newMass.push(el);
+  });
+  for (let i = 0; i < arrs.length; i++) {
+    if (Array.isArray(arrs[i])) {
+      newMass.push(...arrs[i]);
+    } else if (MyArray.isMyArray(arrs[i])) {
+      for (let j = 0; j < arrs[i].length; j++) {
+        newMass.push(arrs[i][j]);
+      }
+    } else {
+      newMass.push(arrs[i]);
     }
   }
-}
+  return newMass;
+};
+
+MyArray.prototype.flat = function (depth = 1) {
+  let newArr = new MyArray();
+
+  this.forEach((el) => {
+    if (MyArray.isMyArray(el) && depth > 0) {
+      newArr = newArr.concat(el.flat(depth - 1));
+    } else if (el !== undefined) {
+      newArr.push(el);
+    }
+  });
+
+  return newArr;
+};
 
 const arr1 = new MyArray(
   0,
